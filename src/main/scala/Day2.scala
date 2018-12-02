f057109c2a0b3424957b2b141b694998d78929f3
Brian Chen <ToucheSir@users.noreply.github.com>
2018-12-01 22:47:26 -0800

    minor cleanups:
    - List[T] can be Seq[T]
    - Use Iterator#next instead of #toSeq.head for maximum laziness

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -7,17 +7,17 @@ object Day2 {
     part2(input)
   }
 
-  def part1(input: List[String]) = {
+  def part1(input: Seq[String]) = {
     val counts = input.map(_.toArray.groupBy(identity).values.map(_.size))
     val hasRepeats = counts.map(c => (c.exists(2.==), c.exists(3.==)))
     val sums = hasRepeats.foldLeft((0, 0))((a, b) => (a._1 + b._1.compare(false), a._2 + b._2.compare(false)))
     println(sums._1 * sums._2)
   }
 
-  def part2(input: List[String]) = {
+  def part2(input: Seq[String]) = {
     val matchingLength = input.head.size - 1
     val combs = input.combinations(2).map(pair => (pair.head zip pair.last))
     val commonLetters = combs.map(_.filter((c1, c2) => c1 == c2).map(_._1).mkString)
-    println(commonLetters.filter(_.size == matchingLength).toSeq.head)
+    println(commonLetters.filter(_.size == matchingLength).next)
   }
 }
